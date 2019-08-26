b0c291a4d9656242f5a639974c393219220bd1d4
Dora <isadorasd@al.insper.edu.br>
2019-08-26 10:07:32 -0300

    tudo

---
+++ sem título1.py
@@ -0,0 +1,11 @@
+#165056629629.62964 - 100%
+#328766962962.96295 - a
+
+a = (328766962962.96295*100) / 165056629629.62964
+print("Aumento percentual dos paises NÃO pertencentes ao SADC (Já com a média):{0}".format(a))
+
+
+#394528142857.1429 - 100%
+#679862857142.8572 - b
+b = (679862857142.8572*100) / 394528142857.1429
+print("Aumento percentual dos paises pertencentes ao SADC (Já com a média):{0}".format(b))
